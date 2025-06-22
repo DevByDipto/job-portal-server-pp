c018414f993ddf4dd8f854715b72ba34baddd535
DevByDipto <dipto.1900@gmail.com>
2025-06-22 15:16:50 +0600

    jwt related practice

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieparser = require("cookie-parser");
 require("dotenv").config();
 const port = process.env.port || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -15,46 +15,29 @@ app.use(
   })
 ); // aivabe e keno likhsi
 app.use(express.json()); // error kheye buja j aita use korte hobe .ar aitar docta khuje ber kora
-app.use(cookieParser());
+app.use(cookieparser()); // aitar kaj kii ? aita kii kora lagbe ?
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nc8opzq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./firebaseAdminkey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
+//verifyToken
 const verifyToken = (req, res, next) => {
-  const token = req?.cookies?.token;
-  console.log("the cookies: ", token);
+  const token = req?.cookies.token;
   if (!token) {
-    return res.status(401).send({ message: "unauthorized access" });
+    return res.status(401).send({ message: "unathorized access..." });
   }
-  // verify
   jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ message: "unauthorized access" });
+      return res.status(401).send({ message: "unathorized access" });
     }
-    req.decoded = decoded;
+    // console.log(decoded.email);
+console.log(decoded);
+
+    req.decoded = decoded; // req a keno set korbo res e noi keno ?
+
+    next();
   });
-  next();
 };
 
-const verifyFirebaseToken = async (req, res, next) => {
-  const authHeader = req?.headers?.authorization;
-  const token = authHeader.split(" ")[1];
-  if (!token) {
-    return res.status(401).send({ message: "unauthorize access" });
-  }
-  const userInfo = await admin.auth().verifyIdToken(token);
-  req.tokenEmail = userInfo.email;
-  // console.log("fb token", userInfo);
 
-  next();
-};
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nc8opzq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -77,31 +60,29 @@ async function run() {
 
     //jwt token related api
     app.post("/jwt", (req, res) => {
-      const userData = req.body;
+      const userData = req?.body;
       const token = jwt.sign(userData, process.env.JWT_ACCESS_SECRET, {
-        expiresIn: "1d",
+        expiresIn: "1h",
       });
-
-      //set token in the cookies
+      // // set token in the cookie
       res.cookie("token", token, {
         httpOnly: true,
         secure: false,
       });
-
+      // console.log(token);
       res.send({ success: true });
     });
 
     // job related api
-
     app.get("/jobs", async (req, res) => {
       const result = await jobsCollection.find().toArray();
-
       res.send(result);
     });
 
     app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
+      // console.log(id);
+
       const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
       res.send(result);
@@ -109,29 +90,16 @@ async function run() {
 
     // job applications related api
 
-    app.get("/applications", verifyFirebaseToken, async (req, res) => {
+    app.get("/applications", verifyToken, async (req, res) => {
       const email = req.query.email;
-      // console.log(req.cookies);
-      // if(email !== req.decoded.email){
-      //   return res.status(403).send({message: "forbidden access"})
-      // }
-
-      if (req.tokenEmail != email) {
-        return res.status(403).send({ message: "forbidden access" });
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden success" });
       }
       const query = {
         applicant: email,
       };
       const result = await applicationsCollection.find(query).toArray();
 
-      // bad way way to agreegate data
-      for (const application of result) {
-        const jobQuery = { _id: new ObjectId(application.jobId) };
-        const appliedjob = await jobsCollection.findOne(jobQuery);
-        application.company = appliedjob.company;
-        application.title = appliedjob.title;
-        application.company_logo = appliedjob.company_logo;
-      }
       res.send(result);
     });
 
